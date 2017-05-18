aaf91995eb2ba148b4f580df9f6f5a2ebd4bc588
Ed Elliott <goeddieuk@gmail.com>
2017-05-18 22:25:17 +0100

    vs 2017 version

--- XmlTestAdapter/Helpers/RunSettings.cs
+++ XmlTestAdapter/Helpers/RunSettings.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 
+
 namespace tSQLtTestAdapter.Helpers
 {
     class RunSettings
